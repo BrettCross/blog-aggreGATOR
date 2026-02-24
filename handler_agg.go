e2b3f13e37e4765b68ea71a368945e8d4bc2af79
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2026-02-24 11:32:06 -0500

    Add feed follow/unfollow commands and RSS aggregation loop

--- handler_agg.go
+++ handler_agg.go
@@ -10,26 +10,39 @@ import (
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.Args) > 0 {
-		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
 	}
 
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
 	if err != nil {
-		return fmt.Errorf("Error fetching feed: %w", err)
+		return fmt.Errorf("Error parsing arg to time: %w", err)
 	}
 
-	fmt.Println(feed.Channel.Title)
-	fmt.Println(feed.Channel.Link)
-	fmt.Println(feed.Channel.Description)
-	for _, item := range feed.Channel.Item {
-		fmt.Println(item.Title)
-		fmt.Println(item.Link)
-		fmt.Println(item.Description)
-		fmt.Println(item.PubDate)
-		fmt.Println()
-		fmt.Println()
+	fmt.Printf("Collecting feeds every %s", &timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
 	}
+
+
+	// feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	// if err != nil {
+	// 	return fmt.Errorf("Error fetching feed: %w", err)
+	// }
+
+	// fmt.Println(feed.Channel.Title)
+	// fmt.Println(feed.Channel.Link)
+	// fmt.Println(feed.Channel.Description)
+	// for _, item := range feed.Channel.Item {
+	// 	fmt.Println(item.Title)
+	// 	fmt.Println(item.Link)
+	// 	fmt.Println(item.Description)
+	// 	fmt.Println(item.PubDate)
+	// 	fmt.Println()
+	// 	fmt.Println()
+	// }
 
 	return nil
 }
@@ -125,5 +138,49 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 	for _, feed := range feeds {
 		fmt.Println(feed.FeedName)
 	}
+	return nil
+}
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <feed_url>", cmd.Name)
+	}
+
+	feedUrl := cmd.Args[0]
+
+	feed, err := s.db.GetFeedByURL(context.Background(), feedUrl)
+	if err != nil {
+		return fmt.Errorf("Error retrieving feed by url: %w", err)
+	}
+
+	s.db.DeleteFeedFollows(context.Background(), database.DeleteFeedFollowsParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+
+	feedToFetch, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("Error fetching next feed: %w", err)
+	}
+
+	_, err = s.db.MarkFeedFetched(context.Background(), feedToFetch.ID)
+	if err != nil {
+		return fmt.Errorf("Error marking feed fetched: %w", err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feedToFetch.Url)
+	if err != nil {
+		return fmt.Errorf("Error fetching feed: %w", err)
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Println(item.Title)
+	}
+
 	return nil
 }
